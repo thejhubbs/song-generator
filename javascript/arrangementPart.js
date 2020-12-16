c952492e2ed00746f19ccc4ce409f1be16acb4e9
Jordan Hubbard <thejhubbs@gmail.com>
2020-12-16 09:43:01 -0500

    Working on genre information & instrument weights

--- javascript/arrangementPart.js
+++ javascript/arrangementPart.js
@@ -44,19 +44,23 @@ class ArrangementPart {
                 chordChart = [chord.printChordNote(1, sn, 6)]
                 break;
             case "bass":
-                chordChart = [chord.printChordNote(1, sn, 2)]
+                chordChart = [chord.printChordNote(Math.round((beatIndex * weightRatio) % 3) + 1, sn, 2)]
                 break;
             case "harmony":
-                chordChart = [chord.printChord(4, sn)]
+                if (weightRatio > .5) {
+                    chordChart = [chord.printChord(4, sn)]
+                } else {
+                    chordChart = [chord.printChordNote(1, sn, 3)]
+                }
                 break;
             case "melody":
-                chordChart = [chord.printChordNote(1, sn, 5)]
+                chordChart = [chord.printScaleNote(Math.round((beatIndex * weightRatio) % 7) + 1, sn, 4)]
                 break;
             case "fx":
-                chordChart = [chord.printChordNote(1, sn, 6)]
+                chordChart = [chord.printChordNote(Math.round((beatIndex * weightRatio) % 3) + 1, sn, 5)]
                 break;
             case "vox":
-                chordChart = [chord.printChordNote(1, sn, 4)]
+                chordChart = [chord.printScaleNote(Math.round((beatIndex * weightRatio) % 7) + 1, sn, 4)]
                 break;
         }
         return chordChart[beatIndex % (chordChart.length)]
@@ -69,19 +73,24 @@ class ArrangementPart {
     //function playMelody() {
     playPart(e, songPartIndex, chord, i, now, time, spacing, mainChord, song) {
         //Apply the instrument weight
-        e = (instrumentWeight[this.name]/100 * e) + songPartIndex
+        e = (instrumentWeight[this.name] / 100 * e) + songPartIndex
         e = bound(e, 1, 10)
 
         let beatPattern = this.beatPattern
         let highestWeight = beatPattern.mainBeat.sort((a, b) => b.weight - a.weight)[0]
         highestWeight = highestWeight ? highestWeight.weight : 1
 
-        let useChord = [true, false]
-        [normalizeAndGetRandomFromMap([beatPattern.resonance, beatPattern.tension])] ?
-            chord :
-            mainChord
+        let useChord = null
 
-        if(this.name === 'harmony') { useChord = chord }
+        let mainChordChoice = ['hat', 'kick', 'fx', 'melody']
+        let chordyChoice = ['harmony', 'bass', 'vox']
+
+        if (chordyChoice.includes(this.name)) { useChord = chord }
+        else if (mainChordChoice.includes(this.name)) { useChord = mainChord }
+        else { useChord = [true, false]
+            [normalizeAndGetRandomFromMap([beatPattern.resonance, beatPattern.tension])] ?
+                chord :
+                mainChord }
 
         beatPattern.mainBeat.map((beatItem, beatIndex) => {
             let p = Number.parseInt(beatItem.position)
@@ -95,15 +104,15 @@ class ArrangementPart {
             let timing = (now + time + (p / spacing))
 
             let weightRatio = w / highestWeight
-            
+
             let melody = this.melody(chord, beatIndex, weightRatio, sn)
 
-            this.instrument.playNote(melody, timing, 1, '8n', weightRatio)
+            this.instrument.playNote(melody, timing, .5 + weightRatio / 2, '8n')
 
         })
     }
 
-    print(){
+    print() {
         let ret = ""
         ret += "<h4>" + this.name + "</h4>"
         ret += this.beatPattern.print()
--- javascript/genre.js
+++ javascript/genre.js
@@ -11,13 +11,13 @@ function getGenreMap(kind) {
                 spread: [4, 6],
                 bpm: [85, 95],
                 instrumentWeight: {
-                    kick: 100,
+                    kick: 200,
                     hat: 100,
-                    bass: 100,
-                    harmony: 100,
-                    melody: 100,
+                    bass: 200,
+                    harmony: 60,
+                    melody: 60,
                     fx: 100,
-                    vox: 100
+                    vox: 60
                 } 
             }
             break;
@@ -32,13 +32,13 @@ function getGenreMap(kind) {
                 bpm: [50, 60],
                 //drums, bass, harmony, melody, fx
                 instrumentWeight: {
-                    kick: 100,
-                    hat: 100,
-                    bass: 100,
-                    harmony: 100,
-                    melody: 100,
-                    fx: 100,
-                    vox: 100
+                    kick: 80,
+                    hat: 80,
+                    bass: 80,
+                    harmony: 200,
+                    melody: 200,
+                    fx: 40,
+                    vox: 200
                 } 
             }
             break;
@@ -46,20 +46,19 @@ function getGenreMap(kind) {
             map = {
                 mode: [1, 1, 2, 4, 5, 6],
                 resonance: [8, 10],
-                tension: [2, 4],
+                tension: [4, 5],
                 repetition: [8, 9],
                 excitement: [8, 10],
                 spread: [4, 6],
-                bpm: [110, 140],
-                //drums, bass, harmony, melody, fx
+                bpm: [120, 140],
                 instrumentWeight: {
-                    kick: 100,
+                    kick: 120,
                     hat: 100,
-                    bass: 100,
-                    harmony: 100,
-                    melody: 100,
-                    fx: 100,
-                    vox: 100
+                    bass: 120,
+                    harmony: 120,
+                    melody: 120,
+                    fx: 120,
+                    vox: 120
                 } 
             }
             break;
@@ -127,25 +126,7 @@ function getGenreMap(kind) {
             }
             break;
     }
-     
-    map = {
-        mode: [1, 1, 2, 4, 5, 6],
-        resonance: [4, 8],
-        tension: [4, 8],
-        repetition: [4, 8],
-        excitement: [6, 8],
-        spread: [4, 6],
-        bpm: [85, 95],
-        instrumentWeight: {
-            kick: 100,
-            hat: 100,
-            bass: 100,
-            harmony: 100,
-            melody: 100,
-            fx: 100,
-            vox: 100
-        } 
-    }
+    
 
     map.mode = randomValueFromArray(map.mode)
     map.resonance = randomValueFromRange(map.resonance)
